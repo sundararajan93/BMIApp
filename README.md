397b64745eb719771ae9d3fe948ca281e86f398a
Sundararajan Thangaraj <geeksundar1993@gmail.com>
2022-10-14 12:41:59 +0530

    Fixed couple of errors and added README.md file

---
+++ README.md
@@ -0,0 +1,27 @@
+# BMI App
+
+BMI App is simple tool built to calculate the BMI based on the below Formula.
+
+
+weight / height<sup>2</sup>
+
+The App has very simple interface with two textboxes to enter 'weight' and 'height'. It has a 'calculate' Button in the bottom. 
+
+![BMI APP](https://i.imgur.com/NBUZz7Q.png)
+
+Once Entered the 'weight' and 'height' information to the textbox hit the 'Calculate' button.
+
+![BMI APP](https://i.imgur.com/6t4BaY7.png)
+
+You would see the BMI Value along with the status of your BMI. Whether it's normal, Underweight or overweight.
+
+![BMI APP](https://i.imgur.com/K3CPBLC.png)
+
+The BMI Value has been calculated based on  below image. 
+
+![BMI VALUE](https://i.imgur.com/P3JXcsD.png)
+
+
+
+
+
--- main.py
+++ main.py
@@ -13,24 +13,30 @@ class MyGridLayout(Widget):
         bmi_weight = self.ids.bmi_weight.text
         h = self.ids.h.text
 
-        BMI_VALUE = (int(bmi_weight) / (int(h)/100.0)**2)
-        ANSWER = (round(BMI_VALUE,2))
+        try:
+            BMI_VALUE = (int(bmi_weight) / (int(h)/100.0)**2)
+            ANSWER = (round(BMI_VALUE,2))
 
-        self.ids.bmi_weight.text = ""
-        self.ids.h.text = ""
-        self.ids.bmi.text = str(ANSWER)
+            self.ids.bmi_weight.text = ""
+            self.ids.h.text = ""
+            self.ids.bmi.text = str(ANSWER)
 
-        if ANSWER <= 18.4:
-            self.ids.bmi.text = str(f"Underweight - {ANSWER}")
-        elif ANSWER >= 18.5 and ANSWER <= 24.9:
-            self.ids.bmi.text = str(f"Normal - {ANSWER}")
-        elif ANSWER >= 25.0 and ANSWER <= 39.9:
-            self.ids.bmi.text = str(f"Overweight - {ANSWER}")
-        elif ANSWER >= 40.0:
-            self.ids.bmi.text = str(f"Obese - {ANSWER}")
-        else:
-            self.ids.bmi.text = str(f"Something is wrong - {ANSWER}")
-        
+            if ANSWER <= 18.4:
+                self.ids.bmi.text = str(f"Underweight - {ANSWER}")
+            elif ANSWER >= 18.5 and ANSWER <= 24.9:
+                self.ids.bmi.text = str(f"Normal - {ANSWER}")
+            elif ANSWER >= 25.0 and ANSWER <= 39.9:
+                self.ids.bmi.text = str(f"Overweight - {ANSWER}")
+            elif ANSWER >= 40.0:
+                self.ids.bmi.text = str(f"Obese - {ANSWER}")
+            else:
+                self.ids.bmi.text = str(f"Something went wrong - {ANSWER}")
+        except:
+            if self.ids.bmi_weight.text == "" or self.ids.h.text == "":
+                self.ids.bmi.text = str(f"All the fields are mandatory")
+            else:
+                self.ids.bmi.text = str(f"Something went wrong")
+
 class BMIApp(App):
     def build(self):
         return MyGridLayout()
